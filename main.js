7fc028d98dd76a61361ea74ec3a21c4db8147dc5
Matthew Wright <matthew.wright5@gmail.com>
2020-03-07 12:51:17 +1100

    add new js scripts

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 let Phrase = require("wrytee-palindrome");
 
-let string = prompt("Please enter a string for palindrome testing:");
-let phrase = new Phrase(string);
+function palindromeTester() {
+  event.preventDefault();
 
-if (phrase.palindrome()) {
-	alert(`"${phrase.content}" is a palindrome!`);
-} else {
-	alert(`"${phrase.content}" is not a palindrome.`);
+  let phrase = new Phrase(event.target.phrase.value);
+  let palindromeResult = document.querySelector("#palindromeResult");
+
+  if (phrase.palindrome()) {
+    palindromeResult.innerHTML = `"${phrase.content}" is a palindrome!`;
+  } else {
+    palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome.`;
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  let tester = document.querySelector("#palindromeTester");
+  tester.addEventListener("submit", function() {
+    palindromeTester(event);
+  });
+});
